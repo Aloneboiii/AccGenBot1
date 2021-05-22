cd0c7972c018bc9b85779eeaf757da93186c73f0
Alain <77918121+infotechbro@users.noreply.github.com>
2021-05-22 11:21:00 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,6 +4,6 @@ class var(object):
     BOT_TOKEN = os.environ.get("TOKEN", None)
     API_HASH = os.environ.get("API_HASH", None)
     APP_ID = int(os.environ.get("API_ID", 6))
-    CHANNEL_USERNAME = os.environ.get("CHANNEL_US", None)
+    CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME", None)
     CHANNEL_URL = os.environ.get("CHANNEL_URL", None)
     LOGS = int(os.environ.get("LOGS_CHAT", False))
