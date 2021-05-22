64ad27490e9452b84cd7f6bbf5cd4449dfbfd7ee
Alain <77918121+infotechbro@users.noreply.github.com>
2021-05-22 16:18:14 +0530

    lel

--- AccGenBot/plugins/start.py
+++ AccGenBot/plugins/start.py
@@ -6,5 +6,5 @@ bot = BotzCity
 
 @bot.on(events.NewMessage(pattern="^[/!]start|Start|START"))
 async def _(event):
-    k = f"""**Heya {event.sender.first_name}, You can generate accounts by using this bot\nUse /cmds or /help to check my commands\n\nDon't forget to join my channel\n\n~ @BotzCity**"
+    k = f"**Heya {event.sender.first_name}, You can generate accounts by using this bot\nUse /cmds or /help to check my commands\n\nDon't forget to join my channel\n\n~ @BotzCity**"
     await bot.send_message(event.chat, k)
